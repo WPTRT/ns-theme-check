eb56f8c15c8b744f39937c1cd7a9d9e8f4c610e7
Denis Žoljom <denis.zoljom@gmail.com>
2019-09-22 11:40:39 +0200

    Update compiled container

--- src/CompiledContainer.php
+++ src/CompiledContainer.php
@@ -4,38 +4,31 @@
  */
 class CompiledContainer extends DI\CompiledContainer{
     const METHOD_MAPPING = array (
-  'Theme_Sniffer\\Admin_Menus\\Sniff_Page' => 'get5d7cfc2a785cc220617420',
-  'Theme_Sniffer\\Api\\Template_Tags_Request' => 'get5d7cfc2a79dc9660528897',
-  'Theme_Sniffer\\Callback\\Run_Sniffer_Callback' => 'get5d7cfc2a79dd2927786611',
-  'Theme_Sniffer\\Enqueue\\Enqueue_Resources' => 'get5d7cfc2a79dd5616357854',
-  'Theme_Sniffer\\i18n\\Internationalization' => 'get5d7cfc2a79dd8669402624',
+  'Theme_Sniffer\\Admin_Menus\\Sniff_Page' => 'get5d873921285f3695255794',
+  'Theme_Sniffer\\Callback\\Run_Sniffer_Callback' => 'get5d8739212b488781548526',
+  'Theme_Sniffer\\Enqueue\\Enqueue_Resources' => 'get5d8739212b6ef442585473',
+  'Theme_Sniffer\\i18n\\Internationalization' => 'get5d8739212b7c5480981663',
 );
 
-    protected function get5d7cfc2a785cc220617420()
+    protected function get5d873921285f3695255794()
     {
         $object = new Theme_Sniffer\Admin_Menus\Sniff_Page();
         return $object;
     }
 
-    protected function get5d7cfc2a79dc9660528897()
-    {
-        $object = new Theme_Sniffer\Api\Template_Tags_Request();
-        return $object;
-    }
-
-    protected function get5d7cfc2a79dd2927786611()
+    protected function get5d8739212b488781548526()
     {
         $object = new Theme_Sniffer\Callback\Run_Sniffer_Callback();
         return $object;
     }
 
-    protected function get5d7cfc2a79dd5616357854()
+    protected function get5d8739212b6ef442585473()
     {
         $object = new Theme_Sniffer\Enqueue\Enqueue_Resources();
         return $object;
     }
 
-    protected function get5d7cfc2a79dd8669402624()
+    protected function get5d8739212b7c5480981663()
     {
         $object = new Theme_Sniffer\i18n\Internationalization();
         return $object;
